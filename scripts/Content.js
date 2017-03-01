4c52f43373e16a940ecca96a0d0553b68cdfa95b
Tala Huhe <tala.huhe@gmail.com>
2017-03-01 03:41:07 +0000

    initial commit

--- scripts/Content.js
+++ scripts/Content.js
@@ -2,6 +2,11 @@ import * as React from 'react';
 
 export class Content extends React.Component {
     render() {
-        return <div><h1>Hello from React!</h1></div>;
+        return (
+            <div>
+                <h1>Hello world!</h1>
+                <h2>Hello second heading!</h2>
+            </div>
+        );
     }
 }
